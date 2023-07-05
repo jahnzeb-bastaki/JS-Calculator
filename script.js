a5829e8a531aebd974d0e20344b94e2ea9edd7c0
Jahnzeb Bastaki <jahnzebbastaki@gmail.com>
2023-07-05 17:04:38 -0500

    Added values to HTML buttons and created basic functions for calculator in script.js

--- script.js
+++ script.js
@@ -1,19 +1,51 @@
 let firstOperand = '';
 let secondOperand = '';
 let operator = '';
-let display = '0';
+let isClear = true;
+let operandFlag = 0; // firstOperand flag = 0 , secondOperand flag = 1;
 
 const screen = document.querySelector('#display');
 const numbers = document.querySelectorAll('.number');
 const functions = document.querySelectorAll('.function');
 const positiveNegative = document.querySelector('#negative');
 const clearButton = document.querySelector('#clear');
+const periodButton = document.querySelector('#period');
+const equalButton = document.querySelector('#equals');
 
+numbers.forEach(number => number.onclick = e => operandLoad(e));
+clearButton.onclick = () => resetCalc();
+
+function resetCalc(){
+  screenDisplay('0');
+  firstOperand = '';
+  secondOperand = '';
+  operator = '';
+}
+
+function screenDisplay(value){
+  screen.textContent = value;
+}
+
+function operandLoad(event){
+  // if operandFlag = firstOperand and there is no operator, 
+  // this means that the calc is in the starting position
+  //
+  // but if the operandFlag is set to 1, that means we need to start
+  // appending to the secondOperand
+  if(!operandFlag && !operator){
+    if(firstOperand == '0' && !operator)
+      screenDisplay(firstOperand = event.target.value);
+    else
+      screenDisplay(firstOperand += event.target.value);
+  } else if(operandFlag){
+    screenDisplay(secondOperand += event.target.value);
+  }
+}
 
 
 
 
 window.onload = () => {
-  screen.innerHTML = display;
-  firstOperand = 0;
+  screenDisplay('0');
+  firstOperand = '0';
 }
